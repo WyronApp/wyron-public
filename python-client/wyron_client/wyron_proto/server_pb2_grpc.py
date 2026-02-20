19258069ca29235785ca5c24374c65392a88dad0
Awake <80032860+oAwake@users.noreply.github.com>
2026-02-20 14:36:00 +0200

    Update server_pb2_grpc.py

--- python-client/wyron_client/wyron_proto/server_pb2_grpc.py
+++ python-client/wyron_client/wyron_proto/server_pb2_grpc.py
@@ -4,7 +4,7 @@ import grpc
 import warnings
 
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
-from wyron_proto import server_pb2 as server__pb2
+from . import server_pb2 as server__pb2
 
 GRPC_GENERATED_VERSION = '1.78.0'
 GRPC_VERSION = grpc.__version__
